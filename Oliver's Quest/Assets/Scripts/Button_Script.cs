f21e5638366f6ba80627c2df6b82d4b5546e2811
Calvin Yoh <flashfire340@gmail.com>
2020-03-19 21:58:19 -0700

    Chapter 1 - Part 1
    Created opening scene as well as interactable buttons

--- Oliver's Quest/Assets/Scripts/Button_Script.cs
+++ Oliver's Quest/Assets/Scripts/Button_Script.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlaceHolder1 : MonoBehaviour
+public class Button_Script : MonoBehaviour
 {
+    public Image theButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        theButton.alphaHitTestMinimumThreshold = 0.0001f;
     }
 
     // Update is called once per frame
--- Oliver's Quest/Assets/Scripts/Main_MenuScript.cs
+++ Oliver's Quest/Assets/Scripts/Main_MenuScript.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main_MenuScript : MonoBehaviour
 {
+    [SerializeField] private Button startButton = null;
+    [SerializeField] private Button chaptersButton = null;
+
+    void Awake()
+    {
+        startButton.GetComponentInChildren<Text>().text = "Start";
+        chaptersButton.GetComponentInChildren<Text>().text = "Chapters";
+    }
     // Start is called before the first frame update
     void Start()
     {
